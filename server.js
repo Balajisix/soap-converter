b9e6539bcba1fff0f0b640e402208a31f506a7d0
Balaji <balajivs0305@gmail.com>
2025-05-07 14:28:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const soap = require("soap");
+const cors = require("cors");
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+
+const service = {
+  CaseInversionService: {
+    CaseInversionServiceSoapPort: {
+      invertCase: function (args) {
+        const input = args.input;
+        const inverted = input
+          .split("")
+          .map((char) =>
+            char === char.toUpperCase()
+              ? char.toLowerCase()
+              : char.toUpperCase()
+          )
+          .join("");
+        return { result: inverted };
+      },
+    },
+  },
+};
+
+const wsdlPath = path.join(__dirname, "caseService.wsdl");
+const xml = fs.readFileSync(wsdlPath, "utf8");
+
+app.listen(PORT, () => {
+  soap.listen(app, "/wsdl", service, xml);
+  console.log(`SOAP server running at ${PORT}`);
+});
+
+module.exports = app;
